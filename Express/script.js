d76f194dae969d53cd42e772ce75134378f3d291
shekhar <shekhart442@gmail.com>
2024-11-22 14:31:37 +0530

    updated express folder

---
+++ Express/script.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next){
+    console.log("Middleware Started...");
+    next();
+});
+
+app.get('/', function(req, res){
+    res.send("Home Page.");
+});
+
+app.get('/about', function(req, res){
+    res.send("About Page.");
+});
+
+app.get('/profile', function(req, res, next){
+    return next(new Error("Error found✅"));
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.listen(3000);
