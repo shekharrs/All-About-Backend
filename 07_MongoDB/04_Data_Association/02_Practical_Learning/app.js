47019205944ba016ef1dd3a9480033b9f829c399
shekhar <shekhart442@gmail.com>
2024-12-05 19:01:21 +0530

    completed the All About Backend Series

--- 07_MongoDB/04_Data_Association/02_Practical_Learning/app.js
+++ 07_MongoDB/04_Data_Association/02_Practical_Learning/app.js
@@ -5,16 +5,32 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const path = require("path");
+const upload = require("./config/multerconfig");
 
 app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// upload route
+app.get("/profile/upload", isLoggedIn , (req, res) => {
+  res.render("profileupload");
+});
+
+// update profile picture --- upload route
+app.post("/upload", isLoggedIn , upload.single("image") , async (req, res) => {
+  let user = await userModel.findOne({email: req.user.email});
+  user.profilepic = req.file.filename;
+  await user.save();
+  res.redirect("/profile");
+})
 
 app.get("/login", (req, res) => {
   res.render("login");
--- 07_MongoDB/04_Data_Association/02_Practical_Learning/models/user.js
+++ 07_MongoDB/04_Data_Association/02_Practical_Learning/models/user.js
@@ -8,6 +8,10 @@ const userSchema = mongoose.Schema({
     password: String,
     email: String,
     age: Number,
+    profilepic: {
+        type: String,
+        default: "default.webp"
+    },
     posts: [
         {
             type: mongoose.Schema.Types.ObjectId,
